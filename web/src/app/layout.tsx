548e049e0446231ebbbaf6acaf42f68d21c6102e
Ayush Sharma <ayush.sharma74892@gmail.com>
2025-05-30 22:49:21 +0530

    errors in analytics

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -5,8 +5,9 @@ import { font } from "@/fonts/font";
 import { Toaster } from "@/components/ui/sonner";
 import { PostHogProvider } from "@/providers/posthog-provider";
 import Script from "next/script";
-import { AnalyticsProvider } from "@/providers/googleanalytics";
+import { GoogleAnalytics } from '@next/third-parties/google'
 import { GA_TRACKING_ID } from "@/lib/gtag";
+
 
 export const metadata: Metadata = {
   title: "Rankify",
@@ -21,29 +22,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
-      <head>
-        {/* Google Analytics - Global Site Tag (gtag.js) */}
-        <Script
-          strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
-        />
-        <Script
-          id="gtag-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', ${GA_TRACKING_ID}, {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
-      </head>
       <body
-        className={`antialiased ${font.className} bg-background text-foreground`}
+        className={` antialiased ${font.className} bg-background text-foreground`}
       >
         <ThemeProvider
           attribute="class"
@@ -52,15 +32,12 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           <Toaster />
-
-          {/* Your custom tracking script */}
           <Script
             defer
-            data-domain="https://rankify-l7e3.onrender.com"
+            data-domain="https://rankify-l7e3.onrender.com" // Replace with your domain
             src="https://analytics-code.vercel.app/tracking-script.js"
           />
-          <AnalyticsProvider />
-
+          <GoogleAnalytics gaId={GA_TRACKING_ID} />
           <PostHogProvider>{children}</PostHogProvider>
         </ThemeProvider>
       </body>
